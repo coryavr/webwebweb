73de909d9272e956061ab0836a016484cb6dba77
coryavr <144748995+coryavr@users.noreply.github.com>
2023-11-27 19:18:58 -0600

    popopopopo

---
+++ Java/main.js
@@ -0,0 +1,14 @@
+console.log("hellow world")
+
+
+let a = 2;
+let b = 3;
+let c = a + b;
+console.log(c)
+
+let myName = "doug"
+console.log(myName)
+
+for(let i=0; i < 100; i++){
+    console.log(i);
+}
